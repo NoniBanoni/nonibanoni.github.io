4cdca6e55bfaa67d5bddf6159620f337ce364776
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2020-01-30 19:08:01 -0800

    Mouse Rectangle Interactive

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let logo;
+// Variables
+let assets = [];
+let database = {};
+let firebase = new Firebase('https://roomonitor-4e09e.firebaseio.com');
+let rects = [];
 
 function preload() {
-  logo = loadImage("assets/Floor Plan.jpg");
+  assets[0] = loadImage("assets/Floor Plan.jpg");
+  assets[1] = loadImage("assets/Floor Plan Simple.jpg");
 }
 
 function setup() {
@@ -9,21 +14,36 @@ function setup() {
 }
 
 function draw() {
+  cursor("crosshair");
   background(255);
-  image(logo, windowWidth / 2 - (min(windowWidth, windowHeight) / 2), windowHeight / 2 - (min(windowWidth, windowHeight) / 2), min(windowWidth, windowHeight), min(windowWidth, windowHeight));
+  image(assets[1], 0, 0);
+  if (mouseIsPressed) {
+    [rects[rects.length - 1][2], rects[rects.length - 1][3]] = [mouseX - rects[rects.length - 1][0], mouseY - rects[rects.length - 1][1]];
+  }
+  noFill();
+  stroke(255, 0, 0);
+  strokeWeight(1);
+  for (let i = 0; i < rects.length; i++) {
+    rect(...rects[i]);
+  }
 }
 
-let data = {};
-let firebase = new Firebase('https://roomonitor-4e09e.firebaseio.com');
+function mousePressed() {
+  rects.push([mouseX, mouseY, 0, 0]);
+}
+
+// Realtime database updates
 firebase.on('child_added', function (snapshot) {
-  data[snapshot.val().id] = json;
+  database[snapshot.val().id] = json;
 });
 firebase.on('child_changed', function (snapshot) {
-  data[snapshot.val().id] = json;
+  database[snapshot.val().id] = json;
 });
 firebase.on('child_removed', function (snapshot) {
   delete data[snapshot.val().id];
 });
+
+// Database edit functions
 function updateChild(name, json) {
   firebase.ref().child(name).update(json);
 }
