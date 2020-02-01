8d7fb1f5b941f13ce2bba0f127ee0341643d1a70
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2020-02-01 10:08:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,96 @@ let firebase = new Firebase('https://roomonitor-4e09e.firebaseio.com');
 let keys = [];
 
 // Other Variables
-let rects =
-  [
-    [254, 0, 189, 96],
-    [449, 0, 191, 93],
-    [646, 0, 106, 93],
-    [657, 93, 95, 120],
-    [657, 219, 95, 173],
-    [660, 402, 91, 105],
-    [655, 520, 96, 199],
-    [649, 576, 6, 143],
-    [325, 146, 104, 125],
-    [276, 271, 153, 50],
-    [203, 328, 202, 118],
-    [203, 452, 82, 67],
-    [292, 453, 37, 29],
-    [202, 526, 82, 67],
-    [327, 588, 34, 47],
-    [368, 586, 62, 80],
-    [437, 586, 61, 80],
-    [505, 586, 73, 61],
-    [505, 653, 74, 66],
-  ];
+let rooms = {
+  "Chat": {
+    "Rects": [
+      [255, 0, 190, 95]
+    ]
+  },
+  "Ideate": {
+    "Rects": [
+      [450, 0, 190, 95]
+    ]
+  },
+  "Make": {
+    "Rects": [
+      [646, 0, 106, 93],
+      [657, 93, 95, 120]
+    ]
+  },
+  "Library": {
+    "Rects": [
+      [657, 219, 95, 173]
+    ]
+  },
+  "Cafe Corner": {
+    "Rects": [
+      [660, 402, 91, 105]
+    ]
+  },
+  "Independence": {
+    "Rects": [
+      [655, 520, 96, 199],
+      [649, 576, 6, 143]
+    ]
+  },
+  "Develop": {
+    "Rects": [
+      [325, 146, 104, 125],
+      [276, 271, 153, 50]
+    ]
+  },
+  "Discover": {
+    "Rects": [
+      [203, 328, 202, 118]
+    ]
+  },
+  "Head Office": {
+    "Rects": [
+      [203, 452, 82, 67]
+    ]
+  },
+  "Connect": {
+    "Rects": [
+      [292, 453, 37, 29]
+    ]
+  },
+  "Curate": {
+    "Rects": [
+      [202, 526, 82, 67]
+    ]
+  },
+  "Communication": {
+    "Rects": [
+      [327, 588, 34, 47]
+    ]
+  },
+  "Coach": {
+    "Rects": [
+      [368, 586, 62, 80]
+    ]
+  },
+  "Mentor": {
+    "Rects": [
+      [437, 586, 61, 80]
+    ]
+  },
+  "Create": {
+    "Rects": [
+      [505, 586, 73, 61]
+    ]
+  },
+  "Explore": {
+    "Rects": [
+      [505, 653, 74, 66]
+    ]
+  },
+  "Commons": {
+    "Rects": [
+      [0, 0, 0, 0]
+    ]
+  },
+};
 
 function preload() {
   assets[0] = loadImage("assets/Floor Plan.jpg");
@@ -44,9 +112,13 @@ function draw() {
   noFill();
   stroke(255, 0, 0);
   strokeWeight(1);
-  for (let i = 0; i < rects.length; i++) {
-    rect(...rects[i]);
-    text(i, rects[i][0] + (rects[i][2] / 2), rects[i][1] + (rects[i][3] / 2));
+  for (let i = 0; i < Object.keys(rooms).length; i++) {
+    let key = Object.keys(rooms)[i];
+    for (let j = 0; j < rooms[key].rects.length; j++) {
+      let roomRect = rooms[key].rects[j]
+      rect(...roomRect);
+      text(keys, roomRect[0] + roomRect[2] / 2, roomRect[1] + roomRect[3] / 2);
+    }
   }
 }
 
