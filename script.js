27cdfa12f6a609488dbe77b8205cfb866e6ccad7
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2020-02-04 09:42:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let fonts = [];
 
 // Other Variables
 let scale = 1.5;
-let rooms = {};
+let rooms = [];
+let dataFetched = false;
 
 function preload() {
   assets[0] = loadImage("assets/Floor Plan.jpg");
@@ -19,36 +20,95 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function displayRooms() {
-  textFont(fonts[0]);
-  noStroke();
-  textAlign(CENTER, CENTER);
-  for (let i = 0; i < Object.keys(rooms).length - 1; i++) {
-    let key = Object.keys(rooms)[i];
-    let x = 0, y = 0, sum = 0;
-    for (let j = 0; j < rooms[key].rects.length; j++) {
-      let roomRect = rooms[key].rects[j];
-      colorMode(HSB);
-      fill((i / Object.keys(rooms).length * 360), 50, 100);
-      colorMode(RGB);
-      rect(roomRect[0] * scale, roomRect[1] * scale, roomRect[2] * scale, roomRect[3] * scale);
-      sum += roomRect[2] * roomRect[3];
-      x += (roomRect[0] + roomRect[2] / 2) * (roomRect[2] * roomRect[3]);
-      y += (roomRect[1] + roomRect[3] / 2) * (roomRect[2] * roomRect[3]);
+function databaseLoaded() {
+  for (let i = 0; i < Object.keys(database.rooms).length - 1; i++) {
+    let key = Object.keys(database.rooms)[i];
+    rooms.push(new Room(database.rooms[key].rects, key));
+  }
+}
+
+class Room {
+  constructor(rects, name) {
+    this.rects = [];
+    for (let i = 0; i < rects.length; i++) {
+      this.rects[i] = new Rectangle(...rects[i]);
     }
-    x /= sum;
-    y /= sum;
-    for (let j = 0; j < rooms[key].rects.length; j++) {
-      let roomRect = rooms[key].rects[j];
-      if (x > roomRect[0] && y > roomRect[1] && x < roomRect[0] + roomRect[2] && y < roomRect[1] + roomRect[3]) {
-        fill(51);
-        textSize(1);
-        textSize(roomRect[2] / textWidth(key) * scale * 0.75);
-        text(key, (roomRect[0] + roomRect[2] / 2) * scale, y * scale);
+    this.name = name;
+  }
+
+  display() {
+    let color = [255, 255, 255];
+
+    for (let i = 0; i < this.rects.length; i++) {
+      if (this.rects[i].mouseOn()) {
+        color = [255, 100, 100];
       }
+    }
+
+    for (let i = 0; i < this.rects.length; i++) {
+      this.rects[i].display(true, color, false);
     }
   }
 }
+
+class Rectangle {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  display(isFilled, fillColor, hasStroke, strokeWidth, strokeColor) {
+    if (!isFilled) {
+      noFill();
+    } else {
+      fill(...fillColor);
+    }
+    if (!hasStroke) {
+      noStroke();
+    } else {
+      strokeWeight(strokeWidth);
+      stroke(...strokeColor);
+    }
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  mouseOn() {
+    return mouseX > this.x && mouseY > this.y && mouseX < this.x + this.w && mouseY < this.y + this.h;
+  }
+}
+
+// function displayRooms() {
+//   textFont(fonts[0]);
+//   noStroke();
+//   textAlign(CENTER, CENTER);
+//   for (let i = 0; i < Object.keys(rooms).length - 1; i++) {
+//     let key = Object.keys(rooms)[i];
+//     let x = 0, y = 0, sum = 0;
+//     for (let j = 0; j < rooms[key].rects.length; j++) {
+//       let roomRect = rooms[key].rects[j];
+//       colorMode(HSB);
+//       fill((i / Object.keys(rooms).length * 360), 50, 100);
+//       colorMode(RGB);
+//       rect(roomRect[0] * scale, roomRect[1] * scale, roomRect[2] * scale, roomRect[3] * scale);
+//       sum += roomRect[2] * roomRect[3];
+//       x += (roomRect[0] + roomRect[2] / 2) * (roomRect[2] * roomRect[3]);
+//       y += (roomRect[1] + roomRect[3] / 2) * (roomRect[2] * roomRect[3]);
+//     }
+//     x /= sum;
+//     y /= sum;
+//     for (let j = 0; j < rooms[key].rects.length; j++) {
+//       let roomRect = rooms[key].rects[j];
+//       if (x > roomRect[0] && y > roomRect[1] && x < roomRect[0] + roomRect[2] && y < roomRect[1] + roomRect[3]) {
+//         fill(51);
+//         textSize(1);
+//         textSize(roomRect[2] / textWidth(key) * scale * 0.75);
+//         text(key, (roomRect[0] + roomRect[2] / 2) * scale, y * scale);
+//       }
+//     }
+//   }
+// }
 
 function draw() {
   background(51);
@@ -59,8 +119,13 @@ function draw() {
     text("FETCHING DATA...", width / 2, height / 2);
     return;
   } else {
-    rooms = database.rooms;
-    displayRooms();
+    if (!dataFetched) {
+      databaseLoaded();
+    }
+    dataFetched = true;
+    for (let i = 0; i < rooms.length; i++) {
+      rooms[i].display();
+    }
   }
 }
 
