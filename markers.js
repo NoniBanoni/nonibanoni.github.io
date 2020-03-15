de7f26f63ecf06deec2e7f9b3eebfd4d2a4b7252
NoniBanoni <41809798+NoniBanoni@users.noreply.github.com>
2020-03-15 11:52:05 -0700

    Update markers.js

--- markers.js
+++ markers.js
@@ -5,6 +5,7 @@ class Marker {
     this.vel = createVector(0, 0);
     this.r = w / 75;
     this.dragged = false;
+    this.loc = "blank";
   }
 
   display() {
@@ -48,7 +49,17 @@ class Marker {
 
     if (this.dragged && mouse.released) {
       this.dragged = false;
+      for (let i = 0; i < rooms.length; i++) {
+        for (let j = 0; j < rooms[i].rects.length; j++) {
+          let temp = rooms[i].rects[j];
+          if (this.pos.x >= temp.pos.x / w && this.pos.y >= temp.pos.y / h && this.pos.x <= (temp.pos.x + temp.w) / w && this.pos.y <= (temp.pos.y + temp.h) / h) {
+            this.loc = rooms[i].name;
+            return;
+          }
+        }
+      }
     }
+
     if (this.name == user.Qt.Ad && !this.dragged) {
       for (let i = 0; i < markers.length; i++) {
         if (dist(markers[i].pos.x * w, markers[i].pos.y * h, this.pos.x * w, this.pos.y * h) < this.r * 2 && markers[i].name !== this.name) {
@@ -59,12 +70,17 @@ class Marker {
           this.vel.add(createVector(v2.x / w, v2.y / h));
         }
       }
-
-      for (let i = 0; i < rooms.length; i++) {
-        for (let j = 0; j < rooms[i].rects.length; j++) {
-          let temp = rooms[i].rects[j];
-          if (this.pos.x >= temp.pos.x / w && this.pos.y >= temp.pos.y / h && this.pos.x <= (temp.pos.x + temp.w) / w && this.pos.y <= (temp.pos.y + temp.h) / h) {
-            this.vel.add(p5.Vector.sub(createVector(rooms[i].center.x / w, rooms[i].center.y / h), this.pos).div(50));
+      if (this.loc != "blank") {
+        for (let i = 0; i < rooms.length; i++) {
+          if (rooms[i].name == this.loc) {
+            for (let j = 0; j < rooms[i].rects.length; j++) {
+              let temp = rooms[i].rects[j];
+              if (this.pos.x >= temp.pos.x / w && this.pos.y >= temp.pos.y / h && this.pos.x <= (temp.pos.x + temp.w) / w && this.pos.y <= (temp.pos.y + temp.h) / h) {
+                return;
+              }
+            }
+            this.vel.add(p5.Vector.sub(createVector(rooms[i].center.x / w, rooms[i].center.y / h), this.pos).div(100));
+            return;
           }
         }
       }
